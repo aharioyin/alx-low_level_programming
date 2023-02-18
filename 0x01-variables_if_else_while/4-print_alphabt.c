95daa97247257cdd74bca33081d06b856dff9958
aharioyin <aharioyinda@gmail.com>
2023-02-17 17:22:20 -0800

    message

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase except letters e and q
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (ch = 'a', ch <= 'z', ch++)
+
+	{
+		if (ch != e && ch != q)
+			putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
