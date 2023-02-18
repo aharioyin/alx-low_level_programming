725db1cf953a3b8c455b82baf6374d04a76d2e3c
aharioyin <aharioyinda@gmail.com>
2023-02-17 18:22:33 -0800

    message

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	int n;
+int n;
 
-	for (n = 0; n < 10; n++)
+for (n = 0; n < 10; n++)
 {
-	printf("%d", n);
+printf("%d", n);
 }
-	printf('\n');
+printf('\n');
 
-	return (0);
+return (0);
 }
