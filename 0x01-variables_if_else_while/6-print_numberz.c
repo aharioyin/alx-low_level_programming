09e2e025c12024821b47e20072acd8872fa01f79
aharioyin <aharioyinda@gmail.com>
2023-02-17 17:44:14 -0800

    message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * using putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+		purchar(n);
+	purchar('\n');
+	return (0);
+}
