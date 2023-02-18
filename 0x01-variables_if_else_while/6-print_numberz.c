92c74694842efbdf6a2e7c3418c420d3c05cd986
aharioyin <aharioyinda@gmail.com>
2023-02-17 18:47:37 -0800

    message

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-	int n;
+	int num;
 
-	for (n = 0; n <= 9; n++)
-		putchar(n);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
 	putchar('\n');
 
 	return (0);
-}
+{
