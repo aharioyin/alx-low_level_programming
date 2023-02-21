ccbb20b00a798ad9376877d2fdb807f91c7f27f0
aharioyin <aharioyinda@gmail.com>
2023-02-21 10:38:50 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,13 @@
 /**
  * _isalpha - checks for alphabetic character.
  * @c:char that is checked
- * Return: 1 if char is an alphabetic character
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 if otherwise
  */
 int _isalpha(int c);
 {
 	if ((c >= 'a' && c <= 'z') ||
-	   (c >= 'A' && c <= 'Z'))
+		(c >= 'A' && c <= 'Z'))
 	return (1);
 
 	else
