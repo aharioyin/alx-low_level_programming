844e5ba4bd8e980a5815d63db3498c47b129fd8c
aharioyin <aharioyinda@gmail.com>
2023-02-21 06:58:57 -0800

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main -  prints the alphabet, in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char low;
+
+	if (low = 'a'; low <= 'z'; low++)
+		_putchar(low);
+
+	_putchar('\n');
+
+	return (0);
+}
