2966bca185c31d1461db62e341c8fd273e419be0
aharioyin <aharioyinda@gmail.com>
2023-02-21 08:35:28 -0800

    message

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_10x - print 10 times the alphabeth in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0, low;
+
+	while (i < 1)
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		_putchar(low);
+		low++;
+	}
