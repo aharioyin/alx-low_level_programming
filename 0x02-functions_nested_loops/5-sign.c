2c18f4f602ee37f3c433e861e57da05f27402c4c
aharioyin <aharioyinda@gmail.com>
2023-02-21 11:46:08 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,8 +10,8 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	{	
-		
+	{
+	
 		_putchar('+');
 		return (1);
 	}
@@ -23,7 +23,7 @@ int print_sign(int n)
 	}
 	else
 	{
-	
+
 		_putchar('0');
 		return (0);
 	}
