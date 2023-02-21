a5c9b5f17bdbecb0ce9d50d5a1ce641ad186f04e
aharioyin <aharioyinda@gmail.com>
2023-02-21 10:18:53 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c:char that is checked
+ * Return: 1 if char is an alphabetic character
+ */
+int _isalpha(int c);
+{
+	for (c >= 'a' || 'A' && c <= 'z' || 'Z')
+	return (1);
+	else
+	return (0);
+}
