99717ee268b26d09cefa188bf1171f94438fe7c8
aharioyin <aharioyinda@gmail.com>
2023-02-23 14:35:04 -0800

    numbers and signs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * print_numbers - prints the numbers, from 0 to 9
+ * Return: void
  */
 
 void print_numbers(void)
@@ -9,7 +10,7 @@ void print_numbers(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i);
+	_putchar(i);
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * print_most_numbers - prints the numbers, from 0 to 9
+ * Return: void
  */
 
 void print_most_numbers(void)
@@ -9,7 +10,7 @@ void print_most_numbers(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i !== '2' && i !== '4')
+		if (i != '2' && i != '4')
 		_putchar(i);
 	}
 	_putchar('\n');
