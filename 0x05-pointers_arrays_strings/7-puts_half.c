6b6b043ea3871a6902f8841e1b37682bcc55d678
aharioyin <aharioyinda@gmail.com>
2023-02-27 18:04:49 -0800

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half -  prints half of a string, followed by a new line.
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int i, a;
+
+	i = 0
+
+	while (str[i] != '\0')
+	i++;
+
+	if (i + 1 % 2 != '0')
+		a = (i - 1) / 2;
+	else
+		a = (i / 2);
+	a++;
+
+	for (i = a; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers, followed by a new line.
+ * @n: number of elements
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+
