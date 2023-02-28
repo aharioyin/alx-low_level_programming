ba82b8a670e37f899f02840f27bb6f6d25defdd1
aharioyin <aharioyinda@gmail.com>
2023-02-27 19:13:20 -0800

    advanced tasks

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,21 @@
 #include "main.h"
 /**
- * print_array - prints n elements of an array of integers, followed by a new line.
+ * print_array - prints n elements of an array of integers
  * @n: number of elements
+ * @a: int to check
  * Return: 0
  */
 
 void print_array(int *a, int n)
 {
+	int b;
 
+	for (b = 0, b < n, n++)
+		if (b != n - 1)
+
+			printf("%d, ", a[b]);
+		else
+			printf("%d", a[b]);
+
+	printf('\n');
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,9 +3,16 @@
  * _strcpy - function that copies the string pointed to by src,
  *	 including the terminating null byte (\0),
  *	 to the buffer pointed to by dest.
+ *@dest: char to check
+ *@src: char to check
  * Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
+	int a;
 
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
