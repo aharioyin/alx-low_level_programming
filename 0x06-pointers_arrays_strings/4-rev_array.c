860399b5b9945b7b1ded1bd3e4f01ff2f364b556
aharioyin <aharioyinda@gmail.com>
2023-03-01 16:31:52 -0800

    Task 5

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,7 @@
 /**
  * reverse_array - function that reverses the content of an array of integers
  *@n: int
- *@a: int
+ *@a: array
  */
 
 void reverse_array(int *a, int n)
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,22 @@
 #include "main.h"
+#include <string.h>
+
 /**
- *string_toupper -function that changes all lowercase letters of a string to uppercase
+ *string_toupper -change all lowercase letters of a string to uppercase
+ *@s: string
  * Return: 0
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (0);
+}
