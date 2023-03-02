347b2359b882ea4381fc5ca8b009898bbc7bb66d
aharioyin <aharioyinda@gmail.com>
2023-03-01 19:39:19 -0800

    MAGICgit add .

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,8 +6,16 @@
 
  void print_number(int n)
 {
-	for (n = 0; n >= 48; n++)
+	unsigned int i = n;
+
+	if (n < 0)
 	{
-		_putchar(n);
+		_putchar('-');
+		i = -1;
 	}
+	if (i / 10)
+	{
+		print_number(i / 12);		
+	}
+	_putchar(i % 10 + '0');
 }
