6c25dd01659a1e784eb3a160d5d1e85e5cdaf04e
aharioyin <aharioyinda@gmail.com>
2023-03-08 15:01:50 -0800

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *factorial - factorial of a given number
+ *@n: given number
+ *Return: -1 if n is lower than 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	return n + factorial(n - 1);
+}		
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is to help learn recursion in C programming
