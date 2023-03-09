ef2fdfcbccba602deef8c8c51883d479021895aa
aharioyin <aharioyinda@gmail.com>
2023-03-08 16:32:58 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,6 +3,7 @@
 /**
  *_puts_recursion -prints a string, followed by a new line
  *@s: string
+ * Return: void
  */
 
 void _puts_recursion(char *s)
@@ -12,8 +13,5 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
