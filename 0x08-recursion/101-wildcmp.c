264438afd60058e04e7a93ed623667284589fefa
aharioyin <aharioyinda@gmail.com>
2023-03-09 18:46:32 -0800

    Hot fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,7 +4,7 @@
  * wildcmp - compares two strings
  *@s1: string
  *@s2: special string
- *Return: 1 if identical or 0 if otherwise 
+ *Return: 1 if identical or 0 if otherwise
  */
 
 int wildcmp(char *s1, char *s2)
@@ -16,8 +16,8 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 
 	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
-		return (wildcmp(s1 + 1, s2 || s2 + 1,s1);
+		return (wildcmp(s1 + 1, s2 || s2 + 1, s1));
 }
