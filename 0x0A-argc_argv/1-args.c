ec95cdd2a6f6acdd538d229f1166221e06bfbdfe
aharioyin <aharioyinda@gmail.com>
2023-03-10 17:52:49 -0800

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main -prints the number of arguments passed into it
+ *@argc: arguments count
+ *@argv: arguments
+ *Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d/n", argc);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ *@argc: argument count
+ *@argv: arguments
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
