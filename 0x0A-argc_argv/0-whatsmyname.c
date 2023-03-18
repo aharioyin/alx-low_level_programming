8bf499b86033da9ce4599699dbd37916f9464a6b
aharioyin <aharioyinda@gmail.com>
2023-03-18 05:16:03 -0700

    argc & argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,7 +11,10 @@
 
 int main(int argc, char **argv)
 {
-	printf("%s\n", argv);
+	(void)argc;
+
+
+	printf("%s\n", *argv);
 
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,21 +7,19 @@
  *@argv: arguments
  *Return: 0
  */
-
 int main(int argc, char **argv)
 {
-	int i, j
+	int x, y;
 
-	if (argc < 3 || argc > 3)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
-
-	printf("%d\n", i * j);
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
 
 	return (0);
 }
