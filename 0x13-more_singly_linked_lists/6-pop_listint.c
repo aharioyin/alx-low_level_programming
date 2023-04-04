02eab3dcf6102e85f3ff3e155148a66f11243250
aharioyin <aharioyinda@gmail.com>
2023-04-04 15:00:09 -0700

    More list functions

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,14 +10,14 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t h;
+	listint_t *h;
 	int n;
 
 	if (*head == NULL || head == NULL)
 		return (0);
 
 	h = *head;
-	*head = h->next;
+	*head = (*head)->next;
 	n = h->n;
 	free(h);
 	return (n);
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	while (j = 0, j < index, j++)
+	for (j = 0; j < index; j++)
 	{
 		head = head->next;
 		if (head == NULL)
