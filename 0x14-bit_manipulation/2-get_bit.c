d71b570e3ec1941e6581464feb47912f6269c300
aharioyin <aharioyinda@gmail.com>
2023-04-10 13:40:47 -0700

    fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int diff, i;
 
-	if (index > (sizeof(unsigned long in t) * 8 - 1))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
 	diff = 1 << index;
