b2d4cf3e0e11200d4b38679ee6011a8e530be23c
aharioyin <aharioyinda@gmail.com>
2023-04-10 13:11:59 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,19 +12,19 @@ unsigned int binary_to_uint(const char *b)
 	int j;
 	unsigned int i = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
 	for (j = 0; b[j] != '\0'; j++)
 	{
-		if (b[j] != '0' && b[j] != '1')
+		if (b[j] != 0 && b[j] != 1)
 			return (0);
 	}
 	for (j = 0; b[j] != '\0'; j++)
 	{
 
 		i <<= 1;
-		if (b[j] == '1')
+		if (b[j] == 1)
 			i += 1;
 	}
 	return (j);
