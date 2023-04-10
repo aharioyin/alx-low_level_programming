8063a383c8af5c69066bc75b0d00201708f66aff
aharioyin <aharioyinda@gmail.com>
2023-04-10 15:00:41 -0700

    Added tasks on File_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,9 +4,31 @@
  *create_file - function that creates a file.
  *@filename: pointer to the name of the file
  *@text_content: pointer to the NULL terminated string to write to the file
- *Return: 1 on success, -1 on failure 
+ *Return: 1 on success, -1 on failure
  *	if filename is NULL return -1
  */
 
 int create_file(const char *filename, char *text_content)
 {
+	int op, writ;
+	int len = 0;
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	if (filename == NULL)
+		return (-1);
+
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	writ = write(op, text_content, len);
+
+	if (op == -1 || writ == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ *_strlen - length of the string
+ *@str: pointer to the string
+ *Return: length of the string
+ */
+
+size_t _strlen(char *str)
+{
+	size_t j;
+
+	for (j = 0; str[j]; j++)
+	;
+
+	return (j);
+}
+
+/**
  *append_text_to_file - appends text at the end of a file
  *@filename: pointer to the name of the file
  *@text_content: pointer to the NULL terminated string to add at the end of the file
@@ -10,3 +26,20 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int ap;
+	ssize_t len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	ap = open(filename, O_WRONLY || O_APPEND);
+	if (text_content != NULL)
+		len = write(ap, text_content, _strlen(text_content));
+	if (ap == -1)
+		return (-1);
+	close(ap);
+	if (len == -1)
+		return (-1);
+	return (-1);
+}
