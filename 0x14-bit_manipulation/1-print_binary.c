8974a4f32d7dd5b88e46a3c98b676b8522117ada
aharioyin <aharioyinda@gmail.com>
2023-04-10 17:45:23 -0700

    Hot fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,3 +4,26 @@
  *print_binary - function that prints the binary representation of a number
  *@n: number
  */
+
+void print_binary(unsigned long int n)
+{
+	int result;
+	unsigned long int i ;
+
+	for (i = n; result = 0; (i >>= 1) > 0; result++)
+		;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (; result >= 0; result--)
+	{
+		if ((n >> result) & 1)
+		printf("1");
+	else
+		printf("0");
+	};
+}
