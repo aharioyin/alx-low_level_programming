f96a75324de4350f1b7a98ffd44d0c3d91f762fb
aharioyin <aharioyinda@gmail.com>
2023-05-18 16:26:51 -0700

    Betty fix in Task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * add_dnodeint_end - function that adds a node 
+ * add_dnodeint_end - function that adds a node
  *		to the end of a linked list
  * @head: double pointer to the beginning of the linked list
  * @n: value to add to new node
